2c5f57e489644ad30c707973166a16d849522b9e
sewlesew <sewlesewbiazen65@gmail.com>
2025-09-22 11:35:22 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
   },
   // Open Graph metadata for social media sharing
   openGraph: {
-    type: "website",
+    type: "website",    
     url: "https://gojoethiopiarestaurantsj.com",
     title: "Gojo Ethiopian Restaurant in San Jose",
     description: "Enjoy authentic Ethiopian food in San Jose. We offer vegan-friendly dishes, traditional flavors, and a cozy cultural dining experience. Call us at (408) 295-9546 to order.",
@@ -91,7 +91,7 @@ export default function RootLayout({
         <title className="s123-js-pjax">GOJO ETHIOPIAN RESTAURANT</title>
         <meta property="og:name" className="s123-js-pjax" content="GOJO ETHIOPIA RESTAURANT" />
         <meta property="og:site_name" className="s123-js-pjax" content="GOJO ETHIOPIAN RESTAURANT" />
-        <meta property="og:image" content="https://gojoethiopiarestaurantsj.com/bg-new.jpg" className="s123-js-pjax" />
+        <meta property="og:image" content="https://gojoethiopiarestaurantsj.com/favicon.ico" />
         <meta property="og:see_also" className="s123-js-pjax" content="https://gojoethiopiarestaurantsj.com" />
         <meta itemProp="name" content="GOJO ETHIOPIA RESTAURANT" className="s123-js-pjax" />
         <meta name="robots" content="all" className="s123-js-pjax" />
